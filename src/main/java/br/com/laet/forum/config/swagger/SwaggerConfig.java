a17c2ed10121ea463bc689960c533c1cd92067fb
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-05-04 22:57:31 -0300

    Adicionado o token jwt à documentação swagger

--- src/main/java/br/com/laet/forum/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/laet/forum/config/swagger/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package br.com.laet.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.laet.forum.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,6 +22,15 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.basePackage("br.com.laet.forum"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(Arrays.asList(
+					new ParameterBuilder()
+					.name("Authorization")
+					.description("Header para token JWT")
+					.modelRef(new ModelRef("string"))
+					.parameterType("header")
+					.required(false)
+					.build()
+				));
 	}
 }
