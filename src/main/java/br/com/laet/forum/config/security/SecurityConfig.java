4e04f74dbcdc108be0a053e9b701ab787797bd4e
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-05-04 21:23:09 -0300

    Implementada a documentação desta API

--- src/main/java/br/com/laet/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/laet/forum/config/security/SecurityConfig.java
@@ -54,6 +54,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(
+				"/**.html", 
+				"/v2/api-docs",
+				"/webjars/**",
+				"/configuration/**",
+				"/swagger-resources/**"
+			);
 	}
 
 }
