5db390d76007fde3c1538c06c60e30a9db74ccae
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-06-18 19:09:57 -0300

    Projeto atualizado e adaptado para a versão 2.5.1 do spring boot

--- src/test/java/br/com/laet/forum/ForumApplicationTests.java
+++ src/test/java/br/com/laet/forum/ForumApplicationTests.java
@@ -1,11 +1,11 @@
 package br.com.laet.forum;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class ForumApplicationTests {
 
